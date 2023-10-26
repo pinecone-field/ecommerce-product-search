29d13429bb71da4ff12a147f60af821c9fa2d4b2
cfossguy <cfossguy@gmail.com>
2023-10-26 12:18:01 -0400

    minor changes.

--- application/app/page.tsx
+++ application/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center p-2 min-h-screen">
       <div className="w-full max-w-screen-md mx-auto flex justify-center items-center"> {/* Center align */}
-        <a href="https://app.pinecone.io/organizations/-NF9xx-MFLRfp0AAuCon/projects/us-east4-gcp:bc4d5e7/indexes/verizon-products/browser" target="_blank">
+        <a href="https://app.pinecone.io/organizations/-NF9xx-MFLRfp0AAuCon/projects/us-east4-gcp:bc4d5e7/indexes/product-catalog/browser" target="_blank">
           <img src="/pinecone-logo-black.png" alt="Semantic Search" className="h-24" />
         </a>
       </div>
